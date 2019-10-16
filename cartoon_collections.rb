7d7b3019942b6dabb859b4d5ac9915e433563346
Anastasia Pankina <siapankina@gmail.com>
2019-10-16 14:56:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(array)
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do|string|
+    string.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
